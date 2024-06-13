cf0f2cd2cc45a16edfc1e11be5be2adf6d65d80b
@d.shurashov <d.shurashov@gmail.com>
2024-06-13 23:10:06 +0300

    leads\components\SearchLeads: warning только при фокусе

--- src/views/leads/components/SearchLeads.vue
+++ src/views/leads/components/SearchLeads.vue
@@ -1,11 +1,13 @@
 <script setup lang="ts">
 import { computed, ref, watch } from 'vue'
 import { WarningOutlined } from '@ant-design/icons-vue'
+import type { ChangeEvent } from 'ant-design-vue/es/_util/EventInterface'
 
 const { onSearch } = defineProps<{ onSearch(query?: string): void }>()
 
 const value = ref<string>('')
-const showWarning = computed(() => value.value.length < 3)
+const isFocused = ref(false)
+const showWarning = computed(() => value.value.length < 3 && isFocused.value)
 
 let timeoutId: ReturnType<typeof setTimeout> | null = null
 
@@ -28,6 +30,13 @@ watch(value, () => {
       </template>
       <WarningOutlined v-if="showWarning" style="color: rgb(250, 140, 22); font-size: 16px" />
     </a-tooltip>
-    <a-input-search v-model:value="value" placeholder="Поиск" style="width: 240px" />
+    <a-input-search
+      :value="value"
+      @input="(event: ChangeEvent) => (value = event.target.value || '')"
+      @blur="() => (isFocused = false)"
+      @focus="() => (isFocused = true)"
+      placeholder="Поиск"
+      style="width: 240px"
+    />
   </a-space>
 </template>
